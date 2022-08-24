2ce82a02bbf62f88d59c6d2bca58c75c7f5e27ce
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-08-23 21:21:29 -0300

    page posts

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,7 +1,44 @@
+import Head from "next/head";
+import styles from './styles.module.scss'
+
 export default function Posts () {
   return(
-    <h1>
-      Posts
-    </h1>
+    <>
+      <Head>
+        <title>Projects - Dev-JeffersonBrito</title>
+      </Head>
+      <main className={styles.mainContainer}>
+        <div className={styles.content}>
+          <div className={styles.cover} >
+            <h2>News about the <br/> 
+            <strong> Front-end </strong> World</h2>
+          </div>
+          <div className={styles.contentPostsList}>
+            <div className={styles.contentBody}>
+              <a href="#" className={styles.cardPost}>
+                <time>12 de março de 2021</time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+              </a>
+              <a href="#" className={styles.cardPost}>
+                <time>12 de março de 2021</time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+              </a>
+              <a href="#" className={styles.cardPost}>
+                <time>12 de março de 2021</time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+              </a>
+              <a href="#" className={styles.cardPost}>
+                <time>12 de março de 2021</time>
+                <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   )
 } 
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -26,7 +26,7 @@ export default function Projects () {
               <a href="#" className={styles.cardProject}>
                 <div className={styles.cardHeader}>
                   <h3 className={styles.cardTitle}>JavaScript data types and data structures</h3>
-                  <span>Há 1 dia</span>
+                  <time>Há 1 dia</time>
                 </div>
                 <p className={styles.cardPreview}>
                 Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
@@ -41,7 +41,7 @@ export default function Projects () {
               <a href="#" className={styles.cardProject}>
                 <div className={styles.cardHeader}>
                   <h3 className={styles.cardTitle}>JavaScript data types and data structures</h3>
-                  <span>Há 1 dia</span>
+                  <time>Há 1 dia</time>
                 </div>
                 <p className={styles.cardPreview}>
                 Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
